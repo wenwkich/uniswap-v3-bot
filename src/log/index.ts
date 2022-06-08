8133d3d998eb992f5561a42fd99076e6d517974d
anonachan <anonachan@prontonmail.com>
2022-06-08 17:14:43 +0800

    logging format

--- src/log/index.ts
+++ src/log/index.ts
@@ -1,9 +1,15 @@
 import { Service } from "typedi";
-import winston from "winston";
-
+import winston, { format } from "winston";
+const { splat, combine, timestamp, label, printf } = format;
 @Service()
 export class LoggerService {
   logger = winston.createLogger({
+    format: combine(
+      timestamp(),
+      printf(({ level, message, timestamp }) => {
+        return `${timestamp} ${level}: ${message}`;
+      })
+    ),
     transports: [
       new winston.transports.Console(),
       new winston.transports.File({ filename: "combined.log" }),
